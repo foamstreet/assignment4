d0cefc2457b49d8f721a7b887a5019cc8618250c
Stacy Mackin <stacy.mackin@gmail.com>
2016-11-30 23:30:27 -0500

    Completed assignment 4.

--- assignment4.js
+++ assignment4.js
@@ -12,6 +12,33 @@
 // keyup events could be helpful to get value of field as the user types
 
 (function() {
-  // Magic!
-  console.log('Keepin\'n it clean with an external script!');
+
+  $('.flexsearch-input').on('keyup', function(){
+    var val = this.value;
+    $.ajax({
+      url: 'http://www.mattbowytz.com/simple_api.json?data=programming',
+      success: function(result) {
+        var predict = [];
+        if(val.length > 0){
+          for(var i = 0; i < result.data.length; i++){
+            if(result.data[i].substring(0, val.length).toLowerCase() === val.toLowerCase()){
+              predict.push(result.data[i]);
+            }
+          }
+        }
+        if(predict.length > 0) {
+          $('#result ul').children().remove();
+          for(var i = 0; i < predict.length; i++) {
+            $('#result ul').append('<li><a href="https://www.google.com/search?q='
+            + predict[i] + '" target="_blank">' + predict[i] + '</a></li>');
+          }
+          $('#result ul').slideDown();
+        }
+        else {
+          $('#result ul').slideUp();
+        }
+      }
+    });
+  });
+
 })();
